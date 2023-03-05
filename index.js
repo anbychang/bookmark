ca73380c33bfc9e77f7700e52dc513acacaea59f
anby <anby924924@gmail.com>
2023-03-05 12:22:20 +0800

    refactor: adjust margins

--- index.js
+++ index.js
@@ -4,18 +4,44 @@ let bookmarks = [
   { name: 'Google', url: 'www.google.com' },
 ]
 
+let id = 0
+
 let main = document.querySelector('#main')
 bookmarks.forEach(bookmark => {
   let a = document.createElement('a')
   a.href = `https://${bookmark.url}`
+  id = id + 1
+  a.id = id
+
   let img = document.createElement('img')
-  if (!bookmark.noIcon) {
-    img.src = `https://${bookmark.url}/favicon.ico`
-  }
+  if (!bookmark.noIcon) {img.src = `https://${bookmark.url}/favicon.ico`}
   a.appendChild(img)
+
   let text = document.createElement('div')
   text.class = 'text'
   text.innerText = bookmark.name
+
   a.appendChild(text)
   main.appendChild(a)
 })
+
+
+function square(l, put_id) {
+  l.forEach(i => {
+  let a = document.createElement('a')
+  a.href = `https://${i.url}`
+  id = id + 1
+  a.id = id
+
+  let img = document.createElement('img')
+  if (!bookmark.noIcon) {img.src = `https://${i.url}/favicon.ico`}
+  a.appendChild(img)
+
+  let text = document.createElement('div')
+  text.class = 'text'
+  text.innerText = bookmark.name
+
+  a.appendChild(text)
+  main.appendChild(a)
+})
+}
